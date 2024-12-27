929dca77ec950a9359cd4bb063da5dedb0e5d58b
Tirth Bodawala <tirthbodawala@atyantik.com>
2024-12-27 23:11:11 +0530

    With rollup output db as library with esm and cjs, removing dependency of noExternal in astro.config.js

---
+++ packages/database/rollup.config.js
@@ -0,0 +1,25 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: 'src/index.ts', // Entry point
+  output: [
+    {
+      file: 'dist/bundle.cjs.js',
+      format: 'cjs', // CommonJS format
+    },
+    {
+      file: 'dist/bundle.esm.js',
+      format: 'esm', // ES Module format
+    },
+  ],
+  plugins: [
+    // Resolves node_modules imports
+    resolve(),       
+    // Converts CommonJS modules to ES6
+    commonjs(),
+    // Compiles TypeScript
+    typescript(),    
+  ],
+};
