179b31f5c94dbc359696180cd97e73637e29eb2e
Tirth Bodawala <tirthbodawala@atyantik.com>
2024-12-28 00:33:03 +0530

    Change path of wrangler platformProxy.persist.path to root folder for cross-package usage

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -5,7 +5,14 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+      persist: {
+        path: "../../.wrangler/state/v3",
+      }
+    }
+  }),
   vite: {
     optimizeDeps: {
       include: ['@services/database'],
