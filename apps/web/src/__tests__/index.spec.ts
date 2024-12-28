b50e33d3bd52ef3f524073b78b8cf0c5563a480f
Tirth Bodawala <tirthbodawala@atyantik.com>
2024-12-28 20:25:06 +0530

    Remove playwright for e2e testing of astro application

--- apps/web/src/__tests__/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("meta is correct", async ({ page }) => {
-  await page.goto("/");
-
-  await expect(page).toHaveTitle(
-    "Cloudflare App with Astro | Atyantik Technologies",
-  );
-});
