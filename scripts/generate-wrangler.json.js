8d1b9c0e590e5c441e16306568950fc166942beb
Tirth Bodawala <tirthbodawala@atyantik.com>
2024-12-30 10:07:15 +0530

    Add .dev.vars as root file and copy the file into packages so variables across all the packages remains consistent

--- scripts/generate-wrangler.json.js
+++ scripts/generate-wrangler.json.js
@@ -1,10 +1,15 @@
 import rootWranglerConfig from '../wrangler.json' with { type: 'json' };
-import { resolve } from 'node:path';
-import { writeFile } from 'node:fs/promises';
+import { dirname, resolve } from 'node:path';
+import { readFile, writeFile } from 'node:fs/promises';
+
+const rootDir = resolve(dirname(new URL(import.meta.url).pathname), '..');
 
 const packageDir = process.cwd();
 const packageWranglerConfigPath = resolve(packageDir, 'wrangler.config.json');
 const outputWranglerConfigPath = resolve(packageDir, 'wrangler.json');
+const rootDevVars = resolve(rootDir, '.dev.vars');
+const rootDevVarsContent = await readFile(rootDevVars, 'utf8');
+const outputDevVarsPath = resolve(packageDir, '.dev.vars');
 
 await (async () => {
   try {
@@ -23,6 +28,7 @@ await (async () => {
       outputWranglerConfigPath,
       JSON.stringify(packageWrangler, null, 2),
     );
+    await writeFile(outputDevVarsPath, rootDevVarsContent);
   } catch (error) {
     console.error(error);
   }
